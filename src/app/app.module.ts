9fcffd0394c7983e497862a7fa395a2412b90ec8
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-11-22 19:47:42 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabezeraComponent } from './componentes/cabezera/cabezera.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabezeraComponent,
+    CuerpoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
